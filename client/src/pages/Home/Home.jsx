d2c617aa406e189379685c3b8ac687ecd3ad1d72
DiasUte <201289@astanait.edu.kz>
2023-04-16 20:08:25 +0600

    FeaturedProducts for HomePage (v:0.5)

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Slider from "../../components/Silder/Slider.jsx";
+import FeaturedProducts from "../../components/FeaturedProducts/FeaturedProducts.jsx";
 
 const Home = () => {
+
+
     return (
         <div>
             <Slider/>
+            <FeaturedProducts type={"Popular Right Now"}/>
+            <FeaturedProducts type={"Trending"}/>
         </div>
     );
 };
