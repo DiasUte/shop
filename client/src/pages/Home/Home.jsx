79d3b55a6a2c98ea55020555455d572ad600d7d7
DiasUte <201289@astanait.edu.kz>
2023-04-16 19:13:15 +0600

    Slider for HomePage (v:0.4)

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Slider from "../../components/Silder/Slider.jsx";
 
 const Home = () => {
     return (
         <div>
-            Home
+            <Slider/>
         </div>
     );
 };
