17e8a8be58f6fb5ccb6f04184e50c1b712c956e6
DiasUte <201289@astanait.edu.kz>
2023-04-16 16:27:40 +0600

    Adding React Router (v:0.1)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,46 @@
+import {BrowserRouter as Router, Route, Routes, Outlet, createBrowserRouter, RouterProvider} from "react-router-dom";
+import Home from "./pages/Home/Home.jsx";
+import Products from "./pages/Products/Products.jsx";
+import Product from "./pages/Product/Product.jsx";
+import Navbar from "./components/Navbar/Navbar.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+
+const Layout = () => {
+    return (
+        <div className="app">
+            <Navbar/>
+            <Outlet/>
+            <Footer/>
+        </div>
+    )
+}
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout/>,
+        children: [
+            {
+                path: "/",
+                element: <Home/>
+            },
+            {
+                path: "/products/:id",
+                element: <Products/>
+            },
+            {
+                path: "/product/:id",
+                element: <Product/>
+            }
+
+        ]
+    }
+])
 
 function App() {
     return (
         <div>
-            Hello world!
+            <RouterProvider router={router}/>
         </div>
     )
 }
